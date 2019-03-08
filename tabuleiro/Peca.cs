949b3132519361387028af40d2906c3a6acebc91
Gelsonmarx <45121804+Gelsonmarx@users.noreply.github.com>
2019-03-08 04:07:24 -0300

    Testes iniciais com movimenos das peças

--- tabuleiro/Peca.cs
+++ tabuleiro/Peca.cs
@@ -1,6 +1,6 @@
 ﻿namespace tabuleiro
 {
-    class Peca
+    abstract class Peca
     {
         public Posicao posicao { get; set; }
         public Cor cor { get; set; }
@@ -14,6 +14,9 @@
             this.cor = cor;
             this.qteMovimentos = 0;
         }
+
+        public abstract bool[,] movimentosPossiveis();
+        
 
         public void incrementarQteMovimentos()
         {
